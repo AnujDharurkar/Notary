e066ca442091a47f81c4c15f460911213fe453b3
AnujDharurkar <adharurkar@gmail.com>
2021-09-12 00:39:23 -0700

    docStats(most and least used words added)

--- src/SearchS.js
+++ src/SearchS.js
@@ -13,9 +13,12 @@ function loadB(filename, displayName) {
     xhr.open("GET", url, true);
     xhr.send();
 
-    xhr.onreadystatechange = function(){
+    xhr.onreadystatechange = function() {
         if(xhr.readyState == 4 && xhr.status == 200){
             currentBook = xhr.responseText;
+
+            debugger;
+            docStats(currentBook);
 
             // remove line breaks and replace wih a <br>
             currentBook = currentBook.replace(/(?:\r\n|\n|\r)/g, '<br>');
@@ -27,4 +30,60 @@ function loadB(filename, displayName) {
 
         }
     };
+}
+
+function docStats(fileContent){
+
+    var docLength = document.getElementById("docLength");
+    var wordCount = document.getElementById("wordCount");
+    var charCount = document.getElementById("charCount");
+
+    let txt = fileContent.toLowerCase();
+    let warray = txt.match(/\b\S+\b/g);
+    let wordDict = {};
+
+    //Count every word
+    for(let word in warray){
+        let wordValue = warray[word];
+        if(wordDict[wordValue] > 0){
+            wordDict[wordValue] += 1;
+        }
+        else{
+            wordDict[wordValue] = 1;
+        }
+    }
+
+    let worList = sortprop(wordDict);
+
+    var top5 = worList.slice(0, 6);
+    var last5 = worList.slice(-6, worList.length);
+
+    //Write values to diplay
+    ULtemp(top5,document.getElementById("mostUsed"));
+    ULtemp(last5,document.getElementById("leastUsed"));
+
+}
+
+function ULtemp(items, element){
+    let rowTemplate = document.getElementById("template-items");
+    let templateHTML = rowTemplate.innerHTML;
+    let results = "";
+    
+    for(let i=0; i < items.length - 1; i++){
+        results += templateHTML.replace('{{val}}',items[i][0] + " : " + items[i][1] + "times(s)");
+    }
+
+    element.innerHTML = results;    
+}
+
+function sortprop(obj){
+    //Convert obj to array
+    let rtnArray = Object.entries(obj);
+
+    //Sort the Array
+    rtnArray.sort(function (first, second){
+        return second[1] - first[1];
+    });
+
+    return rtnArray;
 }
